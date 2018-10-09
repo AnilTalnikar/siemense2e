e479c5db11c382bb27a6de455a2886493cce5648
aniltalnikar <aniltalnikar@yahoo.com>
2018-10-09 14:59:10 +0530

    Changes in main

--- src/main/java/com/vinsys/e2eapp/E2eappApplication.java
+++ src/main/java/com/vinsys/e2eapp/E2eappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class E2eappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Inside Main....");
 		SpringApplication.run(E2eappApplication.class, args);
 	}
 }
